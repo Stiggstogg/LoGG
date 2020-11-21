ebb7f48d60ec2cfb6e3fd29bc3e39d25e9aee005
Fabian Hobi <fabian.hobi@bluewin.ch>
2020-11-21 18:19:38 +0100

    Added positioning of target inside the shootable box, but it is not fully working yet.

--- js/game.js
+++ js/game.js
@@ -12,6 +12,15 @@ gameScene.init = function() {
     // is cursors hidden
     this.cursorHide = false;
 
+    // line properties
+    this.lineWidth = 5;         // width of the lines (in px)
+    this.lineColor = 0x27ff00;  // colors of the lines
+    this.vertLinePos = 0.2;     // relative y position of the vertical line (above is the title) (relative to game width)
+    this.horLinePos = 0.8;      // relative x position of the hrizontal line (to the left the coordinates are shown) (relative to game height)
+
+    // target properties
+    this.targetSize = 100;      // size of the target (in px as the asset is 1 px)
+
 };
 
 // load assets (executed once after init() and before create())
@@ -44,6 +53,22 @@ gameScene.create = function () {
 
     }, this);
 
+    // add lines
+    // ---------------------
+
+    // rectangle (border)
+    let rect = new Phaser.Geom.Rectangle(this.lineWidth*0.5, this.lineWidth*0.5, this.gw-this.lineWidth, this.gh-this.lineWidth); // x, y, width, height
+
+    let graphics = this.add.graphics({lineStyle: {width: this.lineWidth, color: this.lineColor }});  // create graphics object
+
+    graphics.strokeRectShape(rect);     // draw rectangle
+
+    // lines
+    let vertLine = new Phaser.Geom.Line(0, this.gh*this.vertLinePos, this.gw, this.gh*this.vertLinePos);
+    let horLine = new Phaser.Geom.Line(this.gw*this.horLinePos, this.gh*this.vertLinePos, this.gw*this.horLinePos, this.gh);
+
+    graphics.strokeLineShape(vertLine);
+    graphics.strokeLineShape(horLine);
 
     // add target sprite
     // ---------------------
@@ -53,15 +78,29 @@ gameScene.create = function () {
 
     // set properties
     this.target.setOrigin(0.5, 0.5);
-    this.target.setScale(100);
+    this.target.setScale(this.targetSize);
 
     // set interactivity
     this.target.setInteractive();
 
+    let targetSizeGameX = this.targetSize / (this.gw * this.vertLinePos - 1.5 * this.lineWidth) * 100;
+    let targetSizeGameY = this.targetSize / (this.gh * (1 - this.horLinePos) - 1.5 * this.lineWidth) * 100;
+
+    console.log(targetSizeGameX);
+    console.log(targetSizeGameY);
+
     this.target.on('pointerdown', function (pointer) {
+
+        let x = Phaser.Math.Between(targetSizeGameX / 2, 100 - targetSizeGameX / 2);
+        let y = Phaser.Math.Between(targetSizeGameY / 2, 100 - targetSizeGameY / 2);
+
+        console.log(x);
+        console.log(y);
+
+        this.target.setPosition(this.CoordGameToCanvas(x,'x'), this.CoordGameToCanvas(y, 'y'));
+
         console.log('HIT!');
 
-        this.target.setPosition(Phaser.Math.Between(0, this.gw), Phaser.Math.Between(0, this.gh));
     }, this);
 
     // keyboard events
@@ -81,6 +120,26 @@ gameScene.create = function () {
     }, this);
 
 };
+
+// calculates from game coordinates (area on which can be shot) to the real coordinates in the canvas
+gameScene.CoordGameToCanvas = function (gameCoord, dim) {
+
+    if (dim === 'x') {
+        return this.lineWidth + gameCoord / 100 * (this.gw * this.horLinePos - 1.5 * this.lineWidth);
+    }
+    else if (dim === 'y') {
+        return this.gh * this.vertLinePos + this.lineWidth * 0.5 + (100 - gameCoord) / 100 * (this.gh * (1 - this.vertLinePos) - 1.5 * this.lineWidth);
+    }
+
+};
+
+// calculates from game coordinates (area on which can be shot) to the real coordinates in the canvas
+gameScene.yCoordGameToCanvas = function (gameY) {
+
+    return this.lineWidth + gameX * (this.gw * this.horLinePos - 1.5 * this.lineWidth) / 100;
+
+}
+
 
 // update method
 gameScene.update = function () {
