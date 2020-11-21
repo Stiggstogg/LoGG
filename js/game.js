2f7475d17581f3da5879a40b3e24ccc1d869f747
Fabian Hobi <fabian.hobi@bluewin.ch>
2020-11-21 15:47:30 +0100

    Core mechanic including hiding cursor added

--- js/game.js
+++ js/game.js
@@ -5,15 +5,80 @@ let gameScene = new Phaser.Scene('Game');
 // initiate scene parameters
 gameScene.init = function() {
 
+    // get game width and height
+    this.gw = this.sys.game.config.width;
+    this.gh = this.sys.game.config.height;
+
+    // is cursors hidden
+    this.cursorHide = false;
+
 };
 
 // load assets (executed once after init() and before create())
 gameScene.preload = function () {
 
+    // load images
+    this.load.image('target', 'assets/images/target.png');              // target image (1px, red)
+    this.load.image('background', 'assets/images/background.png');      // background image (10x10 px, black)
+
 };
 
 // create objects (executed once after preload())
 gameScene.create = function () {
+
+    // add background sprite (all black)
+    // ----------------------------------
+
+    // create
+    this.bg = this.add.sprite(0, 0, 'background');
+
+    // set properties
+    this.bg.setOrigin(0, 0);
+    this.bg.setScale(this.gw*0.1, this.gh*0.1);
+
+    // set interactivity
+    this.bg.setInteractive();
+    this.bg.on('pointerdown', function (pointer) {
+
+        console.log('miss'); // TODO: Replace with correct action!
+
+    }, this);
+
+
+    // add target sprite
+    // ---------------------
+
+    // create
+    this.target = this.add.sprite(this.gw*0.5, this.gh*0.5, 'target');
+
+    // set properties
+    this.target.setOrigin(0.5, 0.5);
+    this.target.setScale(100);
+
+    // set interactivity
+    this.target.setInteractive();
+
+    this.target.on('pointerdown', function (pointer) {
+        console.log('HIT!');
+
+        this.target.setPosition(Phaser.Math.Between(0, this.gw), Phaser.Math.Between(0, this.gh));
+    }, this);
+
+    // keyboard events
+    // --------------------
+
+    this.keySpace = this.input.keyboard.addKey('Space');
+    this.keySpace.on('down', function (event) {
+
+        if (this.cursorHide) {
+            this.input.setDefaultCursor('default');
+        } else {
+            this.input.setDefaultCursor('none');
+        }
+
+        this.cursorHide = !this.cursorHide;
+
+    }, this);
 
 };
 
